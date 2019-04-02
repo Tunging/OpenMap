bfc3090c914cb52ab48fddd247c28b75fe702c27
jingxuan <185705892@qq.com>
2019-04-03 01:00:47 +0800

    删掉ts文件，防止自动生成，现有代码被替换掉

--- pages/ToMap/ToMap.js
+++ pages/ToMap/ToMap.js
@@ -100,33 +100,4 @@ Page({
       inputAddress :e.detail.value
     })
   },
-  /**
-   * var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
-var qqmapsdk;
-Page({
-
-    onLoad: function () {
-        // 实例化API核心类
-        qqmapsdk = new QQMapWX({
-            key: '申请的key'
-        });
-    },
-    onShow: function () {
-        // 调用接口
-        qqmapsdk.search({
-            keyword: '酒店',
-            success: function (res) {
-                console.log(res);
-            },
-            fail: function (res) {
-                console.log(res);
-            },
-        complete: function (res) {
-            console.log(res);
-        }
-    });
-
-
-})
-   */
 })
